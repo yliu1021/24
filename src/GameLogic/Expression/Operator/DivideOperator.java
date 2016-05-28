18e584fe9f3c42f4a0c20e18e295eb2c2b62ab1b
Albert Zhang <aalbertzhang@gmail.com>
2016-05-28 14:32:00 -0400

    created 4 operators

---
+++ src/GameLogic/Expression/Operator/DivideOperator.java
@@ -0,0 +1,15 @@
+package GameLogic.Expression.Operator;
+
+import GameLogic.Expression.Operand.Operand;
+
+/**
+ * Created by Haimo Zhang on 5/28/2016.
+ */
+public class DivideOperator implements Operator{
+
+
+    @Override
+    public Operand apply(Operand a, Operand b) {
+        return null;
+    }
+}
