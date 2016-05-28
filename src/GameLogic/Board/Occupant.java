4d10ffd85d6ed9e3bf9025cbda1cbb9e165d8f39
Vonais <kevinho688@hotmail.com>
2016-05-28 17:14:46 -0400

    kevins stuff

---
+++ src/GameLogic/Board/Occupant.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author James
+ */
+public interface Occupant {
+    public Location getLocation();
+    public void setLocation(Location x);
+}
