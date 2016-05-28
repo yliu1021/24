c163df06f204b566f32dfc5f1d3bd2df29bd324b
Yuhan Liu <yliu1021@hotmail.com>
2016-05-28 14:54:58 -0400

    Added Operator

--- src/GameLogic/Expression/Operator/Operator.java
+++ src/GameLogic/Expression/Operator/Operator.java
@@ -6,5 +6,5 @@ import GameLogic.Expression.Operand.Operand;
  * Created by Yuhan on 5/28/16.
  */
 public interface Operator {
-    Operand apply(Operand a, Operand b);
+    public Operand apply(Operand a, Operand b);
 }
